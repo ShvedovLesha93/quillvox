dbab0ca9a05ee5aaaf129fe3c5d96e2ed528dc61
Aleksey <shvedovlesha0763s@gmail.com>
2025-12-22 15:01:52 +0300

    feat(ui): add file menu bar with "Open Media" menu item

--- app/views/main_window.py
+++ app/views/main_window.py
@@ -1,17 +1,33 @@
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QApplication
+from PySide6.QtWidgets import (
+    QFileDialog,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+    QApplication,
+)
+
+from app.views.menu_bar import MenuBar
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self.setup_ui()
 
-    def setup_ui(self):
+        self.menu_bar = MenuBar(self)
+        self._setup_ui()
+
+    def _setup_ui(self) -> None:
         central_widget = QWidget()
 
         self.setCentralWidget(central_widget)
         layout = QVBoxLayout()
         central_widget.setLayout(layout)
+
+    def open_file_dialog(self) -> tuple[str, str]:
+        file_dialog = QFileDialog()
+        file, filter = file_dialog.getOpenFileName(self, caption="Open media file")
+
+        return file, filter
 
 
 if __name__ == "__main__":
---
+++ app/views/menu_bar.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+from PySide6.QtCore import QObject
+from PySide6.QtGui import QAction
+
+if TYPE_CHECKING:
+    from app.views.main_window import MainWindow
+
+
+class MenuBar(QObject):
+    def __init__(self, main_window: "MainWindow"):
+        super().__init__()
+        self.main_window = main_window
+        self.menubar = main_window.menuBar()
+        self.file_menu()
+
+    def file_menu(self):
+        file_menu = self.menubar.addMenu("File")
+
+        # Open media
+        self.open_media = QAction("Open Media")
+        file_menu.addAction(self.open_media)
+        self.open_media.triggered.connect(self.main_window.open_file_dialog)
