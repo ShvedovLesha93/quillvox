12f88e2c720f29d1b65fd30575618a2ce5673b41
Aleksey <shvedovlesha0763s@gmail.com>
2026-01-28 13:59:35 +0300

    feat(stt): save transcript to json file during transcription

--- app/models/main_model.py
+++ app/models/main_model.py
@@ -1,8 +1,8 @@
-from app.models.stt_transcript import STTTranscript
+from app.models.transcript import Transcript
 from app.models.file_data import SupportedFormats
 
 
 class MainModel:
     def __init__(self):
         self.file_formats = SupportedFormats()
-        self.stt_transcript = STTTranscript()
+        self.stt_transcript = Transcript()
--- app/view_model/transcript_vm.py
+++ app/view_model/transcript_vm.py
@@ -1,20 +1,52 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-
 from PySide6.QtCore import QObject, Signal
+import json
+from app.models.transcript import STTSegment
 
 if TYPE_CHECKING:
-    from app.view_model.stt_worker_vm import STTWorkerViewModel
+    from faster_whisper.transcribe import Segment, TranscriptionInfo
+    from app.models.transcript import Transcript
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class TranscriptViewModel(QObject):
     segment_str = Signal(str)
 
-    def __init__(self, stt_vm: STTWorkerViewModel):
+    def __init__(self, transcript: Transcript):
         super().__init__()
-        self.vm = stt_vm
+        self.transcript = transcript
         self.buf_text = []
-        self.vm.segment_sent.connect(self.get_segment)
 
-    def get_segment(self, segment: str) -> None:
-        self.segment_str.emit(segment)
+    def clear_transcription(self) -> None:
+        self.transcript.segments.clear()
+        logger.info("transcript.json cleared")
+        with open("transcript.json", "w", encoding="utf-8") as f:
+            f.write("")
+
+    def continue_transcription(
+        self,
+    ) -> None:  # TODO: realize this feature. 2026-01-28 11:59
+        pass
+
+    def on_info(self, info: TranscriptionInfo) -> None:
+        self.transcript.language = info.language
+
+    def on_segment(self, seg: Segment) -> None:
+        segment = STTSegment(
+            id=seg.id,
+            start=seg.start,
+            end=seg.end,
+            text=seg.text,
+        )
+        self.transcript.segments.append(segment)
+        self.segment_str.emit(segment.text.strip())
+        self._save_to_json()
+
+    def _save_to_json(self) -> None:
+        data_json = json.dumps(self.transcript.to_dict(), ensure_ascii=False, indent=2)
+        with open("transcript.json", "w", encoding="utf-8") as f:
+            f.write(data_json)
